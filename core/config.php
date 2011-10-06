4e11dd13ec2a000ffbda6e7b548e7815c5d9f162
Millagou Benjamin <millagou.benjamin@gmail.com>
2011-10-06 13:09:13 +0200

    formattage du fichier de config

--- core/config.php
+++ core/config.php
@@ -1,10 +1,9 @@
 <?php
 
-class config{
+class config {
 
 	//Niveau de déboguage (0 = Prod, >1 = Dev)
 	static $debug = 1;
-
 	//Instanciation des données de connexion aux base de données
 	static $databases = array(
 		'default' => array(
@@ -14,14 +13,11 @@ class config{
 			'password' => ''
 		)
 	);
-
 	//Le controller chargé par défaut si PATH_INFO n'est pas défini
 	static $default_controller = 'posts';
-
 	//L'action appelée par défaut si PATH_INFO n'est pas défini
 	static $default_action = 'index';
 
 }
-
 ?>
 
